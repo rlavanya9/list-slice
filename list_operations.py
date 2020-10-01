6fc3863ed107b688899ca15e59dd2309265f20d1
lavanya <rangaswamy.lavanya@gmail.com>
2020-10-01 23:29:57 +0000

    replace 3rd and last function completed

--- list_operations.py
+++ list_operations.py
@@ -132,6 +132,7 @@ def replace_head(input_list):
     True
 
     """
+    input_list[0] = 42
 
     pass
 
@@ -147,7 +148,9 @@ def replace_third_and_last(input_list):
     True
 
     """
-
+    input_list[2] = 37
+    input_list[-1] = 37
+    
     pass
 
 
